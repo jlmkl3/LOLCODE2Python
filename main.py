782669a5490071c30d6a33b14b12040ea875375b
Julia <juliamikula3@gmail.com>
2026-05-27 11:36:28 +0200

    main

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import antlr4
+from antlr4 import *
 from generated.LolcodeLexer import LolcodeLexer
 from generated.LolcodeParser import LolcodeParser
+from transpiler import Lolcode2Python
 import sys
+
+def main():
+    if len(sys.argv)<2:
+        print("Przy wywoływaniu programu dodaj nazwe pliku wejściowego!")
+        sys.exit()
+
+    file_path=sys.argv[1]
+
+    strumien=FileStream(file_path,encoding='utf-8')
+
+    lekser=LolcodeLexer(strumien)
+    tokeny=CommonTokenStream(lekser)
+    parser=LolcodeParser(tokeny)
+
+    tree=parser.program()
+
+    kod_python=Lolcode2Python(tokeny).visit(tree)
+
+    with open("kod_python.py","w", encoding="utf-8") as f:
+        f.write(kod_python)
+
+if __name__=='__main__':
+    main()
